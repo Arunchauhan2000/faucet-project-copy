bafee8305d5b1177b388846b7ee12dc72ab962dd
Ubuntu <ubuntu@ip-172-31-29-156.ap-south-1.compute.internal>
2025-07-19 11:10:08 +0000

    update

--- models/key.js
+++ models/key.js
@@ -3,8 +3,8 @@ const { Schema } = mongoose;
 
 const keySchema = new Schema({
   workerId: { type: Number, required: true, unique: true, index: true },
-  encryptedKey: { type: String, required: true }
+  encryptedKey: { type: String, required: true },
+  walletAddress: { type: String, required: true }
 });
 
-// Teesra argument 'keys' yeh sunishchit karta hai ki collection ka naam 'keys' hi ho.
-module.exports = mongoose.model('Key', keySchema, 'keys');
+module.exports = mongoose.model('Key', keySchema, 'faucet_key');
